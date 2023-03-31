15ea3385be45dd5d26eb35506373948b702f5fe3
godcandidate <godcandidate@gmail.com>
2023-03-31 09:04:58 +0000

    update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 int main() {
 
-    Users user1;
-    user1.setUser(1, "Eddie", "Administrator", "Admin", "pas123", "02567490");
+    Users user1, user2;
+    user1.setUser(1, "Shaukat Bint", "Administrator", "Admin", "pas123", "02567490");
+    user2.setUser(2, "Denzel Nii", "Cashier", "Denz", "w23frac", "024567191");
 
 
   return 0;
