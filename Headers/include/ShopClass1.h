d802a2e8490b782347b1b5dda2d295b2d4b86feb
godcandidate <godcandidate@gmail.com>
2023-03-31 09:11:54 +0000

    update ShopClass.h

--- Headers/include/ShopClass1.h
+++ Headers/include/ShopClass1.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <iostream>
-#include <fstream>
 using namespace std;
 
 class ShopClass
@@ -16,11 +15,7 @@ class ShopClass
         ShopClass();
         void display();
         void saveHeader(ofstream&);
-        void openfile(string);
-
     protected:
-        ofstream myfile; //stores an open file
-        bool is_fileEmpty;
 
 
 };
@@ -35,16 +30,18 @@ class Users:ShopClass
         void saveLogins();
         void saveUser();
 
+
     protected:
         string username;
         string password;
+
+
 
     public:
         void setUser(int, string, string, string, string, string);
         void editLogin(string, string);
 
         void display();
-
 };
 
 class Products:Users
