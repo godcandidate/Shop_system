be9c4e35657cf08a430f30dc9c7cd4064fbe3e48
godcandidate <godcandidate@gmail.com>
2023-03-31 10:36:53 +0000

    update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -10,6 +10,9 @@ int main() {
     user1.setUser(1, "Shaukat Bint", "Administrator", "Admin", "pas123", "02567490");
     user2.setUser(2, "Denzel Nii", "Cashier", "Denz", "w23frac", "024567191");
 
+    Products product1, product2;
+    product1.addProduct(1, "Indomie",10.00,50);
+    product2.addProduct(2, "Egg",2.00,10);
 
   return 0;
 }
