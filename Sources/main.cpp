3144a9df70943e876eabca7102e728f5834a5820
godcandidate <godcandidate@gmail.com>
2023-04-01 12:31:02 +0000

    update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 
 int main() {
-
 /*
+
     Users user1, user2;
     user1.setUser(1, "Shaukat Bint", "Administrator", "Admin", "pas123", "02567490");
     user2.setUser(2, "Denzel Nii", "Cashier", "Denz", "w23frac", "024567191");
@@ -16,11 +16,11 @@ int main() {
     Products product1, product2;
     product1.addProduct(1, "Indomie",10.00,50);
     product2.addProduct(2, "Egg",2.00,10);
-*/
 
+*/
     Users user1;
     user1.searchUser("Shaukat");
+    user1.editLogins("1", "admin", "ADmin");
 
   return 0;
 }
-
