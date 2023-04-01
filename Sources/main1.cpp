a896c430e26bb963f2f79888190b13cbbc30f3d1
godcandidate <godcandidate@gmail.com>
2023-04-01 07:29:15 +0000

    update main.cpp

--- Sources/main1.cpp
+++ Sources/main1.cpp
@@ -1,4 +1,3 @@
-
 #include "ShopClass.h"
 #include <iostream>
 using namespace std;
@@ -7,19 +6,13 @@ using namespace std;
 
 int main() {
 
-/*
     Users user1, user2;
     user1.setUser(1, "Shaukat Bint", "Administrator", "Admin", "pas123", "02567490");
     user2.setUser(2, "Denzel Nii", "Cashier", "Denz", "w23frac", "024567191");
 
-
     Products product1, product2;
     product1.addProduct(1, "Indomie",10.00,50);
     product2.addProduct(2, "Egg",2.00,10);
-*/
-
-    Users user1;
-    user1.searchUser("Shaukat");
 
   return 0;
 }
